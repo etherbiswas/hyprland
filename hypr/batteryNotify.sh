60b98bd9ba86fbd4db1f88090db6820069f210c8
Ether <etherrorcode404@gmail.com>
2023-02-21 13:51:30 +0600

    updates

---
+++ hypr/batteryNotify.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+while true
+do
+
+    if [ -d /sys/class/power_supply/BAT* ]; then
+        CHARGE=$(cat /sys/class/power_supply/BAT*/capacity)
+        STATUS=$(cat /sys/class/power_supply/BAT*/status)
+        #CHARGE=$(upower -i $(upower -e | grep '/battery') | grep -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+        #STATE=$(upower -i $(upower -e | grep '/battery') | grep  -E state)
+        #STATUS=$(echo "${STATE}" | grep -wo "charging")
+    else
+        exit
+    fi
+
+       if [ "$STATUS" = "Charging" ]; then
+   if [ "$CHARGE" = "90" ]; then
+       notify-send --urgency=CRITICAL "Disconnect Charger" "Level: ${CHARGE}%"
+       paplay /usr/share/sounds/freedesktop/stereo/service-login.oga
+   fi
+
+else
+        if [ "$STATUS" = "Discharging" ]; then
+    if [ "$CHARGE" = "30" ]; then
+        notify-send --urgency=CRITICAL "Connect Charger" "Level: ${CHARGE}%"
+        paplay /usr/share/sounds/freedesktop/stereo/service-logout.oga
+        fi
+
+    else
+            if [ "$STATUS" = "Discharging" ]; then
+        if [ "$CHARGE" = "25" ]; then
+                notify-send --urgency=CRITICAL "Shutting Down!" "Level: ${CHARGE}%"
+                paplay /usr/share/sounds/freedesktop/stereo/service-logout.oga
+                systemctl poweroff
+        fi
+             fi
+    fi
+fi
+sleep 60s
+done
--- install.sh
+++ install.sh
@@ -1,12 +1,42 @@
-#!/bin/bash
-pacman -S base-devel
-# Before this you need base-devel installed
-git clone https://aur.archlinux.org/yay-bin
-cd yay-bin
-makepkg -si
-yay -S hyprland-bin hyprpaper-git polkit-gnome ffmpeg neovim viewnior       \
-rofi pavucontrol pcmanfm fish wl-clipboard wf-recorder     \
-grimblast-git ffmpegthumbnailer tumbler playerctl      \
-noise-suppression-for-voice pcmanfm alacritty\
-waybar-hyprland wlogout swaylock-effects sddm-git pamixer     \
-nwg-look-bin dunst github-desktop-bin brave-bin
+# Pacman
+base-devel
+
+# Yay
+hyprland-bin
+hyprpaper-git
+polkit-gnome
+ffmpeg
+neovim
+viewnior
+rofi
+pavucontrol
+pcmanfm
+fish
+wl-clipboard
+wf-recorder
+grimblast-git
+ffmpegthumbnailer
+tumbler
+playerctl
+noise-suppression-for-voice
+pcmanfm
+alacritty
+waybar-hyprland
+swaylock-effects
+sddm-git
+pipewire
+pamixer
+alsa-utils
+nwg-look-bin
+dunst
+github-desktop-bin
+brave-bin
+exa
+ttf-firacode-nerd
+lxappearance
+wl-color-picker
+wlsunset
+brightnessctl
+xdg-desktop-portal
+xdg-desktop-portal-wlr
+update-grub
